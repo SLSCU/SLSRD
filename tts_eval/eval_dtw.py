cd6b12b89318f0243c8ddf806a992b2b58c56cf1
thcktw <time.not.wait.me@gmail.com>
2020-05-07 13:41:08 +0700

    change color map in plot

--- tts_eval/eval_dtw.py
+++ tts_eval/eval_dtw.py
@@ -29,9 +29,9 @@ def plot_path(gt, pred, path, start_a=0, start_b=0, **plt_kwargs):
         ax1.add_artist(con)
 
     ax1.set_ylabel("ground truth")
-    ax1.imshow(gt)
+    ax1.imshow(gt, cmap='inferno')
     ax2.set_ylabel("prediction")
-    ax2.imshow(pred)
+    ax2.imshow(pred, cmap='inferno')
     plt.show()
 
 @njit
